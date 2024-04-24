b03003cc6bd7bfb803f5832daf87aa511c55e5b3
Phillip  Friedel <s0557917@htw-berlin.de>
2024-04-24 12:20:56 +0200

    Implemented fetching and disyplaying of basic categorys and articles

--- src/components/category_visualizer.jsx
+++ src/components/category_visualizer.jsx
@@ -1,79 +1,82 @@
-"use client"; 
+"use client";
 
 import { useState } from "react";
 import LetterButtons from "./letter_buttons";
 
 export default function CategoryVisualizer() {
-    const [selectedLetter, setSelectedLetter] = useState('')
-    const [selectedLink, setSelectedLink] = useState('')
-    const [results, setResults] = useState([])
+  const [selectedLetter, setSelectedLetter] = useState("");
+  const [selectedLink, setSelectedLink] = useState("");
+  const [results, setResults] = useState([]);
 
-    async function handleClick(clickedLetter) {
-        setSelectedLetter(clickedLetter);   
-        console.log("CLICKED LETTER :: ", clickedLetter)
-  
-        const response = await fetch(`/api/categories/${clickedLetter}`)
-        if( response.status === 200 ) {
-            const data = await response.json()
-            const categories = data.map(entry => {
-                return <div className='m-1 rounded-md p-2 bg-gray-400'> 
-                            <div className="flex space-x-1">
-                                <p className='text-sm font-bold'>{entry.title}</p>
-                            </div>
-                       <button onClick={() => setSelectedLink(`https://en.wikipedia.org/wiki/${entry.title}`)} className="text-xs text-[#646cff]">{"Go ->"}</button>
-                </div>
-            })
+  async function handleClick(clickedLetter) {
+    setSelectedLetter(clickedLetter);
 
-            setResults(categories)
-        } else {
-            console.log("500 STATUS ", response.status)
-        }
-        // const mod = await import(`./data/${clickedLetter}.js`);
-  
-        // const results = Object.entries(mod.default).map(([key, value]) => {
-        //   var key = Object.keys(value)[0]
-        //   var value = Object.values(value)[0]
-  
-        //   return <div className='m-1 rounded-md p-2 bg-gray-400'> 
-        //     <div className="flex space-x-1">
-        //       <p className='text-sm font-bold'>{key}</p>
-        //       {/* <a href={`https://en.wikipedia.org/wiki/${key}`} className="text-sm">{"Go ->"}</a> */}
-        //       <button onClick={() => setSelectedLink(`https://en.wikipedia.org/wiki/${key}`)} className="text-xs text-[#646cff]">{"Go ->"}</button>
-        //     </div>
-        //     <ul>
-        //       {value.map(entry => 
-        //         <div className='flex space-x-1'> 
-        //           <li key={entry} className="text-xs">{entry}</li>
-        //           <button onClick={() => setSelectedLink(`https://en.wikipedia.org/wiki/${entry}`)} className="text-xs text-[#646cff]">{"Go ->"}</button>
-        //         </div>
-        //       )}
-        //     </ul>
-        //   </div>
-        // });
-        //setResults(results)
+    const response = await fetch(`/api/categories/${clickedLetter}`);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log("DATA -- ", data);
+      const categories = data.map((entry) => {
+        return (
+          <div className="m-1 rounded-md p-2 bg-gray-400">
+            <div className="flex space-x-1">
+              <p className="text-sm font-bold">{entry.title}</p>
+              <button
+                onClick={() =>
+                  setSelectedLink(
+                    `https://en.wikipedia.org/wiki/${entry.title}`
+                  )
+                }
+                className="text-xs text-[#646cff]"
+              >
+                {"Go ->"}
+              </button>
+            </div>
+            <div className="flex flex-col space-y-2">
+              {parseArticles(entry.categories_articles)}
+            </div>
+          </div>
+        );
+      });
+
+      setResults(categories);
+    } else {
+      console.log("500 STATUS ", response.status);
     }
+  }
 
-    return (
-        <div className=' flex flex-col flex-grow h-full'>
-            <div>
-                <h1>Wiki Explorer</h1>
-                <LetterButtons handleClick={handleClick}/>
-                <p>Selected letter: {selectedLetter}</p>
-            </div>
-            <p className='bg-gray-100'>Results:</p>
-            <div className='flex bg-gray-100 py-2 flex-1 overflow-y-auto w-full h-full'>
-                <div className="flex flex-grow">
-                <div className='flex flex-col w-1/2 flex-1'>
-                    <div className='flex-1 overflow-y-auto'>
-                        {results}
-                    </div>
-                </div>
-                <div className='w-1/2 flex-1'>
-                    <iframe src={selectedLink ? selectedLink : "https://en.wikipedia.org"} height={"100%"} width={"100%"}/>
-                </div>
-                </div>
-            </div>
+  function parseArticles(articles) {
+    var sorted = articles?.sort((entry) => entry.articles.first_letter);
+    return sorted.map((entry) => {
+      return (
+        <button className="text-xs bg-gray-100 rounded-md p-2 text-left hover:bg-gray-200">
+          {entry.articles.title}
+        </button>
+      );
+    });
+  }
 
+  return (
+    <div className=" flex flex-col flex-grow h-full">
+      <div>
+        <h1>Wiki Explorer</h1>
+        <LetterButtons handleClick={handleClick} />
+        <p>Selected letter: {selectedLetter}</p>
+      </div>
+      <p className="bg-gray-100">Results:</p>
+      <div className="flex bg-gray-100 py-2 flex-1 overflow-y-auto w-full h-full">
+        <div className="flex flex-grow">
+          <div className="flex flex-col w-1/2 flex-1">
+            <div className="flex-1 overflow-y-auto">{results}</div>
+          </div>
+          <div className="w-1/2 flex-1">
+            <iframe
+              src={selectedLink ? selectedLink : "https://en.wikipedia.org"}
+              height={"100%"}
+              width={"100%"}
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/api/categories/[letter].js
+++ src/pages/api/categories/[letter].js
@@ -1,31 +1,37 @@
- 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
-  const prisma = new PrismaClient()
-  const { letter } = req.query 
+  const prisma = new PrismaClient();
+  const { letter } = req.query;
 
   async function fetchCategoriesByLetter() {
     const categories = await prisma.categories.findMany({
       skip: 0,
-      take: 500,
+      take: 1,
       where: {
         first_letter: {
-          equals: letter
-        }
-      }
-    })
-    return categories
+          equals: letter,
+        },
+      },
+      include: {
+        categories_articles: {
+          include: {
+            articles: true,
+          },
+        },
+      },
+    });
+    return categories;
   }
-  
+
   try {
-    const categories = await fetchCategoriesByLetter()
-    console.log("CATS: ", categories)
-    res.status(200).json(categories)
+    const categories = await fetchCategoriesByLetter();
+    console.log("CATS: ", categories);
+    res.status(200).json(categories);
     //await prisma.$disconnect()
   } catch (err) {
-    console.error(e)
-    res.status(500).json({ error: 'failed to load data' })
+    console.error(err);
+    res.status(500).json({ error: "failed to load data" });
     //await prisma.$disconnect()
   }
 }
