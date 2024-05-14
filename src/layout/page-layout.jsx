d068e1933a0a04c90f835708a0b12a641e05c23b
Phillip Friedel <phillipfriedel@gmail.com>
2024-05-14 21:14:27 +0200

    Changed to dvh to fix mobile height issue

--- src/layout/page-layout.jsx
+++ src/layout/page-layout.jsx
@@ -4,8 +4,8 @@ import Navbar from "./navbar";
 export default function PageLayout({ lateralMenu, mainContent }) {
   return (
     <Stack
-      h={"100vh"}
-      mah={"100vh"}
+      h={"100dvh"}
+      mah={"100dvh"}
       align="stretch"
       justify="space-between"
       gap="xs"
